29a8e5df34c258a60f1c4aa556d927b97d1f1e94
nacho <nacho.mota@gmail.com>
2022-12-04 23:08:11 +0100

    new colors & backgaund

--- inc/colors.h
+++ inc/colors.h
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 00:37:54 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/11/10 00:43:47 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2022/12/04 23:07:05 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,35 @@
 # define CYAN	"\033[0;36m"
 # define WHITE	"\033[0;37m"
 # define ORANGE "\033[1;31m"
+# define BLACK "\x1B[30m"
 
 // # Bold
 
 # define BBLACK "\033[1;30m"
-# define BRED "\033[1;31m"
-# define BGREEN "\033[1;32m"
 # define BYELLOW "\033[1;33m"
 # define BBLUE "\033[1;34m"
 # define BMAGENTA "\033[1;35m"
 # define BCYAN "\033[1;36m"
 # define BWHITE "\033[1;37m"
+# define BORANGE "\033[1;31m"
 
 // # Underline
 
-# define URED "\033[4;31m"
-# define UGREEN "\033[4;32m"
+# define UORANGE "\033[4;31m"
 # define UYELLOW "\033[4;33m"
 # define UBLUE "\033[4;34m"
 # define UMAGENTA "\033[4;35m"
 # define UCYAN "\033[4;36m"
 # define UWHITE "\033[4;37m"
+
+// # Background
+# define BGBLACK "\033[40m"
+# define BGRED "\033[41m"
+# define BGGREEN "\033[42m"
+# define BGYELLOW "\033[43m"
+# define BGBLUE "\033[44m"
+# define BGMAGENTA "\033[45m"
+# define BGCYAN "\033[46m"
+# define BGWHITE "\033[47m"
 
 #endif
---
+++ test/color.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 21:40:58 by nmota-bu          #+#    #+#             */
+/*   Updated: 2022/12/04 23:04:57 by nmota-bu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ╔════════════════════════════════════════════════════════════════════════╗ */
+/* ║                 https://github.com/nach131/42Barcelona                 ║ */
+/* ╚════════════════════════════════════════════════════════════════════════╝ */
+
+#include "../inc/colors.h"
+#include "../inc/ft_printf.h"
+
+int main(void)
+{
+
+	ft_printf(UWHITE "Regular Colors\n");
+	ft_printf(RED "This is RED\n");
+	ft_printf(GREEN "This is GREEN\n");
+	ft_printf(YELLOW "This is YELLOW\n");
+	ft_printf(BLUE "This is BLUE\n");
+	ft_printf(MAGENTA "This is MAGENTA\n");
+	ft_printf(CYAN "This is CYAN\n");
+	ft_printf(WHITE "This is WHITE\n");
+	ft_printf(ORANGE "This is ORANGE\n" WHITE);
+
+	ft_printf(UWHITE "\nBold Colors\n");
+	ft_printf(BBLACK "This is BBLACK\n" WHITE);
+	ft_printf(BYELLOW "This is YELLOW\n");
+	ft_printf(BBLUE "This is BLUE\n");
+	ft_printf(BMAGENTA "This is MAGENTA\n");
+	ft_printf(BCYAN "This is CYAN\n");
+	ft_printf(BORANGE "This is BORANGE\n");
+	ft_printf(BWHITE "This is WHITE\n");
+
+	ft_printf(UWHITE "\nUnderline Colors\n");
+	ft_printf(UYELLOW "This is YELLOW\n");
+	ft_printf(UBLUE "This is BLUE\n");
+	ft_printf(UMAGENTA "This is MAGENTA\n");
+	ft_printf(UCYAN "This is CYAN\n");
+	ft_printf(UORANGE "This is UORANGE\n");
+	ft_printf(UWHITE "This is WHITE\n");
+
+	ft_printf(UWHITE "\nBackground Colors\n");
+	ft_printf(RED BGYELLOW "This is RED BGYELLOW\n" BGBLACK);
+	ft_printf(BWHITE BGRED "This is BWHITE BGRED\n" BGBLACK);
+	ft_printf(BBLACK BGGREEN "This is BBLACK BGGREEN \n" BGBLACK);
+	ft_printf(BORANGE BGBLUE "This is BORANGE BGBLUE  \n" BGBLACK);
+	ft_printf(BYELLOW BGBLUE "This is BYELLOW BGBLUE  \n" BGBLACK);
+	ft_printf(BGRED BYELLOW "This is BGRED BYELLOW\n" BGBLACK);
+	ft_printf(BGBLACK WHITE "---------------------\n");
+}
